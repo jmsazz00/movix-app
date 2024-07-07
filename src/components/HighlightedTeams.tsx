46dba2fec3c257ec794fe3c3e7f378bbe6904697
jmsazz00 <jamesazzi193@gmail.com>
2024-07-07 23:29:46 +0300

    Adding card skeleton for loading state

--- src/components/HighlightedTeams.tsx
+++ src/components/HighlightedTeams.tsx
@@ -1,32 +1,39 @@
-import { Grid, Paper, CircularProgress } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import useTeams from "../hooks/useTeams";
 import TeamCard from "./TeamCard";
+import TeamCardSkeleton from "./TeamCardSkeleton";
 
 const HighlightedTeams = () => {
   const { data: teams, isLoading, error } = useTeams();
 
-  if (isLoading) return <CircularProgress />;
+  const skeletonCount = [1, 2, 3, 4, 5, 6];
 
   if (error) return error;
 
   return (
     <Grid container spacing={3}>
-      {teams?.teams?.map((team) => (
-        <Grid item xs={12} sm={6} md={4} key={team.idTeam}>
-          <Paper
-            elevation={3}
-            sx={{
-              p: 2,
-              minHeight: "400px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "stretch",
-            }}
-          >
-            <TeamCard teamData={team} />
-          </Paper>
-        </Grid>
-      ))}
+      {isLoading
+        ? skeletonCount.map((c) => (
+            <Grid item xs={12} sm={6} md={4} key={c}>
+              <TeamCardSkeleton />
+            </Grid>
+          ))
+        : teams?.teams?.map((team) => (
+            <Grid item xs={12} sm={6} md={4} key={team.idTeam}>
+              <Paper
+                elevation={3}
+                sx={{
+                  p: 2,
+                  minHeight: "400px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "stretch",
+                }}
+              >
+                <TeamCard teamData={team} />
+              </Paper>
+            </Grid>
+          ))}
     </Grid>
   );
 };
---
+++ src/components/TeamCardSkeleton.tsx
@@ -0,0 +1,27 @@
+import { Card, Skeleton, CardContent } from "@mui/material";
+
+const TeamCardSkeleton = () => {
+  return (
+    <Card sx={{ maxWidth: 400, margin:"auto" }}>
+      <Skeleton variant="rectangular" height={300} animation="wave" />
+      <CardContent>
+        <Skeleton animation="wave" height={20} style={{ marginBottom: 12 }} />
+        <Skeleton
+          animation="wave"
+          height={20}
+          width="80%"
+          style={{ marginBottom: 12 }}
+        />
+        <Skeleton
+          animation="wave"
+          height={20}
+          width="60%"
+          style={{ marginBottom: 12 }}
+        />
+        <Skeleton animation="wave" height={20} width="90%" />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TeamCardSkeleton;
