f3d85c5d1bb2941e395ab40cb81d2ff0c00dec56
jmsazz00 <jamesazzi193@gmail.com>
2024-07-16 23:54:02 +0300

    Adding expandable text collapse transition

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -1,26 +1,44 @@
-import { Alert, Button } from "@mui/material";
-import { useState } from "react";
+import { Alert, Button, Collapse } from "@mui/material";
+import { useEffect, useState } from "react";
 
 interface Props {
   text: string;
   limit: number;
+  parentId: string;
 }
 
-const ExpandableText = ({ text, limit }: Props) => {
+const ExpandableText = ({ text, limit, parentId }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+  const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    if (!expanded && !initialRender) {
+      const element = document.getElementById(parentId);
+      setTimeout(() => {
+        window.scroll({ top: element?.offsetTop, behavior: "smooth" });
+      }, 450);
+    }
+  }, [expanded, initialRender]);
+
   if (!text) return <Alert severity="error">Not currently available</Alert>;
 
-  if (text.length <= limit) return text;
-
-  const [expanded, setExpanded] = useState(false);
-  const trimmed = text.slice(0, limit) + "...";
+  if (text.length <= limit) return <div>{text}</div>;
 
   return (
-    <>
-      {expanded ? text : trimmed}
-      <Button size="small" onClick={() => setExpanded(!expanded)}>
+    <div>
+      <Collapse in={expanded} collapsedSize={limit}>
+        <div>{text}</div>
+      </Collapse>
+      <Button
+        size="small"
+        onClick={() => {
+          setExpanded(!expanded);
+          if (initialRender) setInitialRender(false);
+        }}
+      >
         {expanded ? "Show Less" : "Read More"}
       </Button>
-    </>
+    </div>
   );
 };
 
--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -27,68 +27,71 @@ const TeamCard = ({ teamData }: Props) => {
     setSelectedLanguage(event.target.value as string);
   };
 
-  const descriptionLimit = 250;
+  const descriptionLimitPx = 100;
 
   return (
-    <Card
-      sx={{
-        maxWidth: 400,
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        m: "0 auto",
-        bgcolor:
-          theme.palette.mode === "light"
-            ? "#fafafa"
-            : theme.palette.background.default,
-      }}
-    >
-      <CardMedia
-        component="img"
-        height="200"
-        image={teamData.strBadge}
-        sx={{ objectFit: "contain", paddingTop: 2 }}
-        alt={teamData.strTeam}
-      />
-      <CardContent>
-        <Typography variant="h5" gutterBottom>
-          {teamData.strTeam}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" paragraph>
-          <strong>Country:</strong> {teamData.strCountry}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <strong>League:</strong> {teamData.strLeague}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <strong>Stadium:</strong> {teamData.strStadium}
-        </Typography>
-        <FormControl sx={{ mt: 2, minWidth: 120 }}>
-          <InputLabel id="language-label">Language</InputLabel>
-          <Select
-            labelId="language-label"
-            value={selectedLanguage}
-            onChange={handleLanguageChange}
+    <div id={teamData.idTeam}>
+      <Card
+        sx={{
+          maxWidth: 400,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          m: "0 auto",
+          bgcolor:
+            theme.palette.mode === "light"
+              ? "#fafafa"
+              : theme.palette.background.default,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="200"
+          image={teamData.strBadge}
+          sx={{ objectFit: "contain", paddingTop: 2 }}
+          alt={teamData.strTeam}
+        />
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            {teamData.strTeam}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" paragraph>
+            <strong>Country:</strong> {teamData.strCountry}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <strong>League:</strong> {teamData.strLeague}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <strong>Stadium:</strong> {teamData.strStadium}
+          </Typography>
+          <FormControl sx={{ mt: 2, minWidth: 120 }}>
+            <InputLabel id="language-label">Language</InputLabel>
+            <Select
+              labelId="language-label"
+              value={selectedLanguage}
+              onChange={handleLanguageChange}
+            >
+              <MenuItem value="EN">English</MenuItem>
+              <MenuItem value="ES">Spanish</MenuItem>
+              <MenuItem value="FR">French</MenuItem>
+            </Select>
+          </FormControl>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            mt={2}
+            textAlign={"justify"}
+            component={"div"}
           >
-            <MenuItem value="EN">English</MenuItem>
-            <MenuItem value="ES">Spanish</MenuItem>
-            <MenuItem value="FR">French</MenuItem>
-          </Select>
-        </FormControl>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          mt={2}
-          textAlign={"justify"}
-          component={"div"}
-        >
-          <ExpandableText
-            text={teamData[`strDescription${selectedLanguage}` as keyof Team]}
-            limit={descriptionLimit}
-          />
-        </Typography>
-      </CardContent>
-    </Card>
+            <ExpandableText
+              text={teamData[`strDescription${selectedLanguage}` as keyof Team]}
+              limit={descriptionLimitPx}
+              parentId={teamData.idTeam}
+            />
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
