6af10ef321838119b6efa01f94af59bccc19bfe1
jmsazz00 <jamesazzi193@gmail.com>
2024-07-17 00:37:47 +0300

    Adding hover effect on countries list

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -4,11 +4,14 @@ import {
   Divider,
   Stack,
   Typography,
+  useTheme,
 } from "@mui/material";
 import useCountries from "../hooks/useCountries";
 
 const CountriesList = () => {
   const { data: list, isLoading, error } = useCountries();
+
+  const theme = useTheme();
 
   if (isLoading) return <CircularProgress />;
   if (error) console.log(error);
@@ -33,11 +36,25 @@ const CountriesList = () => {
   return (
     <Stack spacing={1} divider={<Divider flexItem />}>
       {selectedCountries?.map((c) => (
-        <Box key={c.name_en} display={"flex"} pl={3}>
+        <Box
+          key={c.name_en}
+          sx={{
+            cursor: "pointer",
+            display: "flex",
+            pl: 3,
+            py: 0.75,
+            transition: "background-color 0.3s ease",
+            "&:hover": {
+              backgroundColor:
+                theme.palette.mode === "light" ? "#f0f0f0" : "#2b333e",
+              "& .MuiTypography-root": {
+                fontWeight: "bold",
+              },
+            },
+          }}
+        >
           <img src={c.flag_32_url} style={styles} />
-          <Typography sx={{ cursor: "pointer", alignSelf: "center" }}>
-            {c.name_en}
-          </Typography>
+          <Typography sx={{ alignSelf: "center" }}>{c.name_en}</Typography>
         </Box>
       ))}
     </Stack>
